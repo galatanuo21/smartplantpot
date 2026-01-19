cf197c5abf9234c55a5b618be5e3b8e271973ce0
olegg <olegg@OLEG>
2026-01-19 10:14:52 +0100

    Projektdateien hinzufügen.

---
+++ SmartPlantPot/UserService.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartPlantPot
+{
+    public class UserService
+    {
+        public string Name { get; set; } = "";
+        public string DeviceID { get; set; } = "";
+        public bool IsLoggedIn { get; set; } = false;
+    }
+}

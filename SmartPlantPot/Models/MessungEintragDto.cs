858acc11c9ce662b01e967522820afcb65bf72c3
Oleg Galatanu <galatanu.o21@htlwienwest.at>
2026-01-29 22:58:05 +0100

    api sendet

---
+++ SmartPlantPot/Models/MessungEintragDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartPlantPot.Models
+{
+    public class MessungEintragDto
+    {
+        public decimal Humidity { get; set; }
+        public string Modus { get; set; }
+    }
+}
--- SmartPlantPot/Models/Messwerte.cs
+++ SmartPlantPot/Models/Messwerte.cs
@@ -8,6 +8,6 @@ namespace SmartPlantPot.Models
         public int Id { get; set; }
         public string Geraete_Id { get; set; }
         public DateTime Timestamp { get; set; }
-        public double Humidity { get; set; }
+        public decimal Humidity { get; set; }
     }
 }

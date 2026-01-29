dd6fc7ae449c93ef80123ce8326fbd1629e828f1
Oleg Galatanu <galatanu.o21@htlwienwest.at>
2026-01-29 21:42:26 +0100

    api daten lesen

--- SmartPlantPot/Models/Messwerte.cs
+++ SmartPlantPot/Models/Messwerte.cs
@@ -1,7 +1,10 @@
-﻿namespace SmartPlantPot.Models
+﻿using System.Text.Json.Serialization;
+
+namespace SmartPlantPot.Models
 {
     public class Messwerte
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
         public string Geraete_Id { get; set; }
         public DateTime Timestamp { get; set; }

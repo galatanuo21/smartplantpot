b9d525981b8ee48d0ebf72d2a7f0c2df70487667
Oleg Galatanu <galatanu.o21@htlwienwest.at>
2026-01-29 20:12:48 +0100

    funktionierbar

--- SmartPlantPot/Models/Messwerte.cs
+++ SmartPlantPot/Models/Messwerte.cs
@@ -3,6 +3,7 @@
     public class Messwerte
     {
         public int Id { get; set; }
+        public string Geraete_Id { get; set; }
         public DateTime Timestamp { get; set; }
         public double Humidity { get; set; }
     }
